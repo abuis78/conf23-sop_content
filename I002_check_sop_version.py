8745ec96e38ee5b3183781e01dc94a20a682e64e
andreas buis <abuis@splunk.com>
2023-07-10 14:20:41 +0000

    ewrwer

--- I002_check_sop_version.py
+++ I002_check_sop_version.py
@@ -579,20 +579,23 @@ def update_version_no_in_list(action=None, success=None, container=None, results
     playbook_input_sop_version = phantom.collect2(container=container, datapath=["playbook_input:sop_version"])
     playbook_input_liste_name = phantom.collect2(container=container, datapath=["playbook_input:liste_name"])
     playbook_input_sop_name = phantom.collect2(container=container, datapath=["playbook_input:sop_name"])
+    filtered_result_0_data_filter_found_sop = phantom.collect2(container=container, datapath=["filtered-data:filter_found_sop:condition_1:finde_sop_in_list:action_result.summary.locations.0.1"])
 
     playbook_input_sop_version_values = [item[0] for item in playbook_input_sop_version]
     playbook_input_liste_name_values = [item[0] for item in playbook_input_liste_name]
     playbook_input_sop_name_values = [item[0] for item in playbook_input_sop_name]
+    filtered_result_0_summary_locations_0_1 = [item[0] for item in filtered_result_0_data_filter_found_sop]
 
     ################################################################################
     ## Custom Code Start
     ################################################################################
 
     # Write your custom code here...
-    phantom.debug(playbook_input_liste_name[0][0])
+    sop_version = playbook_input_sop_version[0][0]
+    sop_name = playbook_input_liste_name[0][0]
     liste_name = playbook_input_liste_name[0][0]
     decided_list_tag_url = phantom.build_phantom_rest_url('decided_list',liste_name)
-    data = { "update_rows": { "0": [playbook_input_sop_name, playbook_input_sop_version]}}
+    data = { "update_rows": { "0": [sop_name, sop_version]}}
     response = phantom.requests.post(decided_list_tag_url,json=data,verify=False)
     ################################################################################
     ## Custom Code End
