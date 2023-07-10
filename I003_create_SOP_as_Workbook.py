98cdf6b887094e499b3fc54fef72fec37a58a651
andreas buis <abuis@splunk.com>
2023-07-10 19:06:00 +0000

    kjh

---
+++ I003_create_SOP_as_Workbook.py
@@ -0,0 +1,39 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+
+
+    return
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    output = {
+        "sop_id": [],
+    }
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.save_playbook_output_data(output=output)
+
+    return
