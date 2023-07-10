722ec33e159cb1acf74e35e01333e49edc57a649
andreas buis <abuis@splunk.com>
2023-07-10 12:17:02 +0000

    ewrwer

--- I002_check_sop_version.py
+++ I002_check_sop_version.py
@@ -12,9 +12,27 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'call_api_1' block
+    call_api_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def call_api_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("call_api_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
