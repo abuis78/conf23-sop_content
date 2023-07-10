c4691e888feedb5b5c9485f3025a70f97e51c70d
andreas buis <abuis@splunk.com>
2023-07-10 08:04:52 +0000

    jjj

--- I001_extract_JSON_from_file.py
+++ I001_extract_JSON_from_file.py
@@ -34,10 +34,10 @@ def get_file_information_extract_content(action=None, success=None, container=No
     # Write your custom code here...
     vault_id = playbook_input_vault_id
     success, message, vault_info = phantom.vault_info(vault_id=vault_id)
-    vault_info = list(vault_info)[0]
-    vault_path = vault_info['path']
+    # vault_info = list(vault_info)[0]
+    # vault_path = vault_info['path']
     phantom.debug("vault_info {}".format(vault_info))
-    phantom.debug("vault_path {}".format(vault_path))
+    #phantom.debug("vault_path {}".format(vault_path))
 
     ################################################################################
     ## Custom Code End
