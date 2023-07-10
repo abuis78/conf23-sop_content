8d9a85267587ebf8af3ced01a70078561dae37fd
andreas buis <abuis@splunk.com>
2023-07-10 14:33:37 +0000

    ewrwer

--- I002_check_sop_version.py
+++ I002_check_sop_version.py
@@ -597,8 +597,9 @@ def update_version_no_in_list(action=None, success=None, container=None, results
     liste_name = playbook_input_liste_name[0][0]
     decided_list_tag_url = phantom.build_phantom_rest_url('decided_list',liste_name)
     data = { "update_rows": { finde_sop_in_list_summary_locations0_0: [sop_name, sop_version]}}
-    response = phantom.requests.post(decided_list_tag_url,json=data,verify=False)
-    phantom.debug("phantom returned status code {} with message {}".format(response.status_code, response.text))
+    phantom.debug(data)
+    #response = phantom.requests.post(decided_list_tag_url,json=data,verify=False)
+    #phantom.debug("phantom returned status code {} with message {}".format(response.status_code, response.text))
     ################################################################################
     ## Custom Code End
     ################################################################################
