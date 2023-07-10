97469a3be80d2dac733efdf5bfad0b2f18df11b5
andreas buis <abuis@splunk.com>
2023-07-10 19:37:24 +0000

    kjh

--- I003_create_SOP_as_Workbook.py
+++ I003_create_SOP_as_Workbook.py
@@ -157,17 +157,18 @@ def delete_sop(action=None, success=None, container=None, results=None, handle=N
 def debug_delete(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("debug_delete() called")
 
-    get_sop_id_for_update_result_data = phantom.collect2(container=container, datapath=["get_sop_id_for_update:action_result.data.*.parsed_response_body","get_sop_id_for_update:action_result.data.*.parsed_response_body.data.0","get_sop_id_for_update:action_result.parameter.context.artifact_id"], action_results=results)
+    get_sop_id_for_update_result_data = phantom.collect2(container=container, datapath=["get_sop_id_for_update:action_result.data.*.parsed_response_body","get_sop_id_for_update:action_result.data.*.parsed_response_body.data.0","get_sop_id_for_update:action_result.data","get_sop_id_for_update:action_result.parameter.context.artifact_id"], action_results=results)
 
     get_sop_id_for_update_result_item_0 = [item[0] for item in get_sop_id_for_update_result_data]
     get_sop_id_for_update_result_item_1 = [item[1] for item in get_sop_id_for_update_result_data]
+    get_sop_id_for_update_result_item_2 = [item[2] for item in get_sop_id_for_update_result_data]
 
     parameters = []
 
     parameters.append({
         "input_1": get_sop_id_for_update_result_item_0,
         "input_2": get_sop_id_for_update_result_item_1,
-        "input_3": None,
+        "input_3": get_sop_id_for_update_result_item_2,
         "input_4": None,
         "input_5": None,
         "input_6": None,
