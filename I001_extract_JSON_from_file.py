9fbd95d58ee08dbd7076fc68115b11991e5b8684
andreas buis <abuis@splunk.com>
2023-07-10 10:34:19 +0000

    ddd

--- I001_extract_JSON_from_file.py
+++ I001_extract_JSON_from_file.py
@@ -36,8 +36,8 @@ def get_file_information_extract_content(action=None, success=None, container=No
         vault_id = playbook_input_vault_id[0]
         success, message, vault_info = phantom.vault_info(vault_id=vault_id[0])
         phantom.debug("vault_info: {}".format(vault_info))
-        path = json.dumps(vault_info[0]["path"]) 
-        name = json.dumps(vault_info[0]["name"]) 
+        path = vault_info[0]["path"]
+        name = vault_info[0]["name"]
         phantom.debug("path: {}".format(path))
         phantom.debug("name: {}".format(name))
         file_path_name = '/'.join([path, name])
