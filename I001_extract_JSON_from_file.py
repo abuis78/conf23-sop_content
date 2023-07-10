c8ed4ff3f9023478c97aaffdfcea50f9cddf1f7d
andreas buis <abuis@splunk.com>
2023-07-10 08:46:04 +0000

    ddd

--- I001_extract_JSON_from_file.py
+++ I001_extract_JSON_from_file.py
@@ -40,7 +40,7 @@ def get_file_information_extract_content(action=None, success=None, container=No
         name = json.dumps(json_data["name"]) 
         phantom.debug("path: {}".format(path))
         phantom.debug("name: {}".format(name))
-        file_path_name = path + "\/" + name
+        file_path_name = path +"/"+ name
         phantom.debug(file_path_name)
         
         with open(file_path_name) as file:
