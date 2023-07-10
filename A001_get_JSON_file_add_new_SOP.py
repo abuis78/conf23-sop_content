c281ea3aba46c574f871ddeb1ce3ae4a456c340b
andreas buis <abuis@splunk.com>
2023-07-10 08:01:36 +0000

    ddd

--- A001_get_JSON_file_add_new_SOP.py
+++ A001_get_JSON_file_add_new_SOP.py
@@ -12,9 +12,34 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'playbook_i001_extract_json_from_file_1' block
+    playbook_i001_extract_json_from_file_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def playbook_i001_extract_json_from_file_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_i001_extract_json_from_file_1() called")
+
+    inputs = {
+        "vault_id": ["\n11606de950a4c482cfcba4038852270427cf1abd"],
+    }
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    # call playbook "conf23-sop_content/I001_extract_JSON_from_file", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("conf23-sop_content/I001_extract_JSON_from_file", container=container, name="playbook_i001_extract_json_from_file_1", inputs=inputs)
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
