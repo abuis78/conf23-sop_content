ea6c67de2fe0bc1d1333092c3fa0e8900b3b0ba5
andreas buis <abuis@splunk.com>
2023-07-10 19:11:04 +0000

    dd

--- A001_get_JSON_file_add_new_SOP.py
+++ A001_get_JSON_file_add_new_SOP.py
@@ -18,162 +18,6 @@ def on_start(container):
     return
 
 @phantom.playbook_block()
-def format_sop_json(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_sop_json() called")
-
-    template = """{{\"cef\":{{\n\"sop_json\": \"{0}\",\n\"version\": 2\n}}\n}}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "format_playbook_output:formatted_data"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_sop_json")
-
-    create_new_sop_artifact(container=container)
-
-    return
-
-
-@phantom.playbook_block()
-def create_new_sop_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("create_new_sop_artifact() called")
-
-    id_value = container.get("id", None)
-    format_sop_name = phantom.get_format_data(name="format_sop_name")
-    format_sop_json = phantom.get_format_data(name="format_sop_json")
-
-    parameters = []
-
-    parameters.append({
-        "name": format_sop_name,
-        "tags": None,
-        "label": None,
-        "severity": None,
-        "cef_field": None,
-        "cef_value": None,
-        "container": id_value,
-        "input_json": format_sop_json,
-        "cef_data_type": None,
-        "run_automation": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="create_new_sop_artifact")
-
-    return
-
-
-@phantom.playbook_block()
-def format_sop_name(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_sop_name() called")
-
-    template = """NEW SOP"""
-
-    # parameter list for template variable replacement
-    parameters = []
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_sop_name")
-
-    format_sop_json(container=container)
-
-    return
-
-
-@phantom.playbook_block()
-def format_playbook_output(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_playbook_output() called")
-
-    template = """{0}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "playbook_i001_extract_json_from_file_1:playbook_output:json_content"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_playbook_output")
-
-    debug_3(container=container)
-
-    return
-
-
-@phantom.playbook_block()
-def debug_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("debug_3() called")
-
-    format_playbook_output = phantom.get_format_data(name="format_playbook_output")
-
-    parameters = []
-
-    parameters.append({
-        "input_1": format_playbook_output,
-        "input_2": None,
-        "input_3": None,
-        "input_4": None,
-        "input_5": None,
-        "input_6": None,
-        "input_7": None,
-        "input_8": None,
-        "input_9": None,
-        "input_10": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_3")
-
-    return
-
-
-@phantom.playbook_block()
 def playbook_i001_extract_json_from_file_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("playbook_i001_extract_json_from_file_3() called")
 
@@ -228,18 +72,60 @@ def playbook_i002_check_sop_version_2(action=None, success=None, container=None,
     ################################################################################
 
     # call playbook "conf23-sop_content/I002_check_sop_version", returns the playbook_run_id
-    playbook_run_id = phantom.playbook("conf23-sop_content/I002_check_sop_version", container=container, name="playbook_i002_check_sop_version_2", callback=add_comment_4, inputs=inputs)
+    playbook_run_id = phantom.playbook("conf23-sop_content/I002_check_sop_version", container=container, name="playbook_i002_check_sop_version_2", callback=decision_1, inputs=inputs)
 
     return
 
 
 @phantom.playbook_block()
-def add_comment_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("add_comment_4() called")
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_1() called")
 
-    playbook_i002_check_sop_version_2_output_sop_in_list_status = phantom.collect2(container=container, datapath=["playbook_i002_check_sop_version_2:playbook_output:sop_in_list_status"])
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["playbook_i002_check_sop_version_2:playbook_output:sop_in_list_status", "==", "update"]
+        ],
+        delimiter=None)
 
-    playbook_i002_check_sop_version_2_output_sop_in_list_status_values = [item[0] for item in playbook_i002_check_sop_version_2_output_sop_in_list_status]
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        comment_existing_sop_is_being_updated(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 2
+    found_match_2 = phantom.decision(
+        container=container,
+        conditions=[
+            ["playbook_i002_check_sop_version_2:playbook_output:sop_in_list_status", "==", "newly_created"]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 2 matched
+    if found_match_2:
+        comment_the_new_sop_is_being_created(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 3
+    found_match_3 = phantom.decision(
+        container=container,
+        conditions=[
+            ["playbook_i002_check_sop_version_2:playbook_output:sop_in_list_status", "==", "nothing_to_do"]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 3 matched
+    if found_match_3:
+        comment_there_is_no_need_for_action(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
+def comment_existing_sop_is_being_updated(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("comment_existing_sop_is_being_updated() called")
 
     ################################################################################
     ## Custom Code Start
@@ -251,7 +137,45 @@ def add_comment_4(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.comment(container=container, comment=playbook_i002_check_sop_version_2_output_sop_in_list_status_values)
+    phantom.comment(container=container, comment="Existing SOP is being updated")
+
+    return
+
+
+@phantom.playbook_block()
+def comment_the_new_sop_is_being_created(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("comment_the_new_sop_is_being_created() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="The new SOP is being created")
+
+    return
+
+
+@phantom.playbook_block()
+def comment_there_is_no_need_for_action(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("comment_there_is_no_need_for_action() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="There is no need for action")
 
     return
 
