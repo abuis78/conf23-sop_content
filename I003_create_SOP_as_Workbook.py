581f0725404fc21326100dbc296c7fde5cd8a594
andreas buis <abuis@splunk.com>
2023-07-10 19:19:49 +0000

    kjh

--- I003_create_SOP_as_Workbook.py
+++ I003_create_SOP_as_Workbook.py
@@ -12,9 +12,88 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'decision_task_type' block
+    decision_task_type(container=container)
 
     return
+
+@phantom.playbook_block()
+def format_endpoint(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_endpoint() called")
+
+    template = """{0}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "playbook_input:sop_json"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_endpoint")
+
+    get_sop_id_for_update(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def get_sop_id_for_update(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("get_sop_id_for_update() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    format_endpoint = phantom.get_format_data(name="format_endpoint")
+
+    parameters = []
+
+    if format_endpoint is not None:
+        parameters.append({
+            "location": format_endpoint,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("get data", parameters=parameters, name="get_sop_id_for_update", assets=["soar_http"])
+
+    return
+
+
+@phantom.playbook_block()
+def decision_task_type(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decision_task_type() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["playbook_input:task", "==", "update"]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        format_endpoint(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
