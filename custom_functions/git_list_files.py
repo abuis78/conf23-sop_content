e6f5d750f73fd6306739649812a619f8519b9aef
andreas buis <abuis@splunk.com>
2023-07-11 19:55:47 +0000

    eee

--- custom_functions/git_list_files.py
+++ custom_functions/git_list_files.py
@@ -20,65 +20,32 @@ def git_list_files(repo_path_local=None, repo_path_remote=None, filter_file_ends
     import subprocess
     import os
 
-    def get_local_git_json_files(repo_path):
-        # Git-Befehl ausführen, um alle Dateien im Repository zu erhalten
-        cmd = ["git", "ls-tree", "--name-only", "-r", "HEAD"]
-        output = subprocess.check_output(cmd, cwd=repo_path).decode().strip()
+    def auflisten_git_verzeichnis(repo_path_local):
+        # Wechseln Sie zum lokalen Repository-Verzeichnis
+        os.chdir(repo_path_local)
 
-        # Die Ausgabe in eine Liste von Dateinamen aufteilen
-        file_list = output.split("\n")
+        # Liste alle Dateien, die vom lokalen Git-Repository verfolgt werden
+        dateien = subprocess.check_output(["git", "ls-files"]).decode("utf8")
+        phantom.debug("Dateien im lokalen Git-Repository:")
+        phantom.debug(dateien)
 
-        # Filtere Dateien mit der Erweiterung ".json"
-        json_files = [file for file in file_list if file.endswith(".json")]
+    def check_git_diff(repo_path_remote):
+        # Holt die neuesten Informationen vom remote repository
+        subprocess.run(["git", "remote", "set-url", "origin", repo_path_remote])
+        subprocess.run(["git", "fetch"])
 
-        return json_files
+        # Überprüft den Unterschied zwischen dem lokalen und dem remote repository
+        result = subprocess.check_output(["git", "diff", "--name-only", "origin/main"]).decode("utf8")
 
-    def is_local_file_older(file_path, repo_path):
-        # Git-Befehl ausführen, um den letzten Commit-Zeitstempel für die Datei zu erhalten
-        cmd = ["git", "log", "-1", "--format=%ct", "--", file_path]
-        output = subprocess.check_output(cmd, cwd=repo_path).decode().strip()
-
-        if output:
-            local_timestamp = int(output)
-            return local_timestamp
+        if result:
+            phantom.debug("\nEs gibt Unterschiede zwischen dem lokalen und dem remote Repository:")
+            phantom.debug(result)
         else:
-            return None
+            phantom.debug("\nKeine Unterschiede gefunden. Das lokale Repository ist aktuell.")
 
-    def is_remote_file_newer(file_path, repo_path):
-        # Git-Befehl ausführen, um den letzten Commit-Zeitstempel für die Datei zu erhalten
-        cmd = ["git", "ls-remote", "--exit-code", "--quiet", "--refs", repo_path, "HEAD"]
-        try:
-            subprocess.check_output(cmd)
-            return True  # Remote-Repository und Branch existieren
-        except subprocess.CalledProcessError:
-            return False  # Remote-Repository oder Branch existieren nicht
 
-    def replace_with_remote_file(file_path, repo_path):
-        # Git-Befehl ausführen, um die Datei mit der Version aus dem Remote-Repository zu ersetzen
-        cmd = ["git", "checkout", "origin/HEAD", "--", file_path]
-        subprocess.run(cmd, cwd=repo_path)
-
-    # Repository-Pfad angeben
-    repo_path = repo_path_local
-
-    # Lokale JSON-Dateien erhalten
-    local_json_files = get_local_git_json_files(repo_path)
-
-    # Überprüfe jede lokale Datei
-    for file in local_json_files:
-        local_file_path = os.path.join(repo_path, file)
-
-        # Überprüfe, ob die lokale Datei älter ist als die Online-Version
-        if is_local_file_older(local_file_path, repo_path):
-            if is_remote_file_newer(file, repo_path_remote):
-                # Die Online-Version ist neuer, ersetze die lokale Datei
-                replace_with_remote_file(file, repo_path_remote)
-                phantom.debug(f"Die lokale Datei '{file}' wurde mit der neueren Online-Version aktualisiert.")
-            else:
-                phantom.debug(f"Die lokale Datei '{file}' ist bereits auf dem neuesten Stand.")
-
-        else:
-            phantom.debug(f"Die lokale Datei '{file}' hat keine Versionsinformationen.")
+    auflisten_git_verzeichnis(repo_path_local)
+    check_git_diff(repo_path_remote)
 
 
         
