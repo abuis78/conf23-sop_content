e16a3b4ab0a33a95bba665c1596aaec745773117
andreas buis <abuis@splunk.com>
2023-07-11 08:48:43 +0000

    eee

--- custom_functions/git_list_files.py
+++ custom_functions/git_list_files.py
@@ -83,7 +83,7 @@ def git_list_files(local_repo_path=None, online_repo_path=None, filter_file_ends
     # Vergleiche die Dateilisten und lade neue Dateien herunter
     for remote_file in remote_json_files:
         if remote_file not in local_json_files:
-            replace_with_remote_file(remote_file, local_repo_path)
+            replace_with_remote_file(remote_file, online_repo_path)
             phantom.debug(f"Die neue Datei '{remote_file}' wurde aus dem Online-Repository heruntergeladen.")
         
         
