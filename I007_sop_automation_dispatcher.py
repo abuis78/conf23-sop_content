4c0fb11a3fb10eebea94d6e23a0cccf70a6abd88
andreas buis <abuis@splunk.com>
2023-07-12 08:14:47 +0000

    dd

--- I007_sop_automation_dispatcher.py
+++ I007_sop_automation_dispatcher.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'add_comment_1' block
-    add_comment_1(container=container)
+    # call 'set_automation_phase' block
+    set_automation_phase(container=container)
 
     return
 
@@ -36,6 +36,28 @@ def add_comment_1(action=None, success=None, container=None, results=None, handl
     ################################################################################
 
     phantom.comment(container=container, comment=playbook_input_automation_phase_values)
+
+    return
+
+
+@phantom.playbook_block()
+def set_automation_phase(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("set_automation_phase() called")
+
+    playbook_input_automation_phase = phantom.collect2(container=container, datapath=["playbook_input:automation_phase"])
+
+    playbook_input_automation_phase_values = [item[0] for item in playbook_input_automation_phase]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+    phase = playbook_input_automation_phase_values[0]
+    success, message = phantom.set_phase(phase=phase)
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
 
