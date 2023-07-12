a76ed19449e54b01c6ad43841b44c01f741d395c
andreas buis <abuis@splunk.com>
2023-07-12 09:02:38 +0000

    ddd

--- U04-Check for login activities on other hosts.py
+++ U04-Check for login activities on other hosts.py
@@ -12,9 +12,33 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'add_comment_1' block
+    add_comment_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("add_comment_1() called")
+
+    playbook_input_current_task = phantom.collect2(container=container, datapath=["playbook_input:current_task"])
+
+    playbook_input_current_task_values = [item[0] for item in playbook_input_current_task]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment=playbook_input_current_task_values)
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
