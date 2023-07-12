ed890654dc1a8f0856c188ba821d67455a4d9cd3
andreas buis <abuis@splunk.com>
2023-07-12 09:41:10 +0000

    yt

--- custom_functions/check_automation_process_hud.py
+++ custom_functions/check_automation_process_hud.py
@@ -16,7 +16,11 @@ def check_automation_process_hud(status=None, container_id=None, phase_id=None, 
     
     # Write your custom code here...
     url_filter = "?_filter_container_id=" + str(container_id) + "&_filter_phase=" + str(phase_id) + "&_filter_status="+ str(status)
-    
+    task_url = phantom.build_phantom_rest_url('workbook_task')
+    url = task_url + url_filter
+    response = phantom.requests.get(url,verify=False)
+    data = response.json()
+    phantom.debug(data["count"])
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
     return outputs
