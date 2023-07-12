b828d49740775d3bebb0236e5d0bc4d5c0209e92
andreas buis <abuis@splunk.com>
2023-07-12 05:43:39 +0000

    eee

--- custom_functions/git_list_files.py
+++ custom_functions/git_list_files.py
@@ -11,45 +11,40 @@ def git_list_files(repo_path_local=None, repo_path_remote=None, filter_file_ends
     ############################ Custom Code Goes Below This Line #################################
     import json
     import phantom.rules as phantom
-
+    import subprocess
+    import os
+    
     outputs = {}
     
     # Write your custom code here...
 
-    # Konfigurieren Sie diese Parameter entsprechend
-    import subprocess
-    import os
+
 
     def auflisten_git_verzeichnis(repo_path_local):
-        # Wechseln Sie zum lokalen Repository-Verzeichnis
         os.chdir(repo_path_local)
 
-        # Liste alle Dateien, die vom lokalen Git-Repository verfolgt werden
         dateien = subprocess.check_output(["git", "ls-files"]).decode("utf8")
-        phantom.debug("Dateien im lokalen Git-Repository:")
+        phantom.debug("Files in the local Git repository:")
         phantom.debug(dateien)
 
     def check_git_diff(repo_path_remote):
-        # Holt die neuesten Informationen vom remote repository
         subprocess.run(["git", "remote", "set-url", "origin", repo_path_remote])
         subprocess.run(["git", "fetch"])
         
-        # Holt die neuesten Informationen vom remote repository
         subprocess.run(["git", "fetch"])
         
-        # Überprüft den Unterschied zwischen dem lokalen und dem remote repository
         result = subprocess.check_output(["git", "diff", "--name-only", "origin/main"]).decode("utf8")
         
         geaenderte_dateien = []
         
         if result:
-            phantom.debug("\nEs gibt Unterschiede zwischen dem lokalen und dem remote Repository:")
-            geaenderte_dateien = result.split('\n')[:-1] # Remove the last element as it's empty
+            phantom.debug("\nThere are differences between the local and remote repository:")
+            geaenderte_dateien = result.split('\n')[:-1] 
             phantom.debug(result)
-            phantom.debug("\nAktualisiere das lokale Repository...")
+            phantom.debug("\nUpdate the local repository...")
             subprocess.run(["git", "pull"])
         else:
-            phantom.debug("\nKeine Unterschiede gefunden. Das lokale Repository ist aktuell.")
+            phantom.debug("\nNo differences found. The local repository is up to date.")
             
         phantom.debug(geaenderte_dateien)
         return geaenderte_dateien
