a1c323e111413fa3f97daa76e23f3bb9c4080c4b
andreas buis <abuis@splunk.com>
2023-07-16 20:18:56 +0000

    kkk

--- custom_functions/git_list_files.py
+++ custom_functions/git_list_files.py
@@ -18,43 +18,23 @@ def git_list_files(repo_path_local=None, pull_response=None, **kwargs):
     outputs = {}
     
     # Write your custom code here...
-    def list_json_files(repo_path_local):
-        json_files = []
-        vault_id_list = []
-        phantom.debug(repo_path_local)
-        for root, dirs, files in os.walk(repo_path_local):
-            for file in files:
-                if file.endswith('.json'):
-                    json_files.append(os.path.join(root, file))
-                    file = os.path.join(root, file)
-                    parts = file.split('/')
-                    name = parts[-1]  
-                    success, message, vault_id = phantom.vault_add(container=None, file_location=file, file_name=name, metadata=None, trace=False)
-                    vault_id_list.append(vault_id)
-                else:
-                    phantom.debug("Fuck off")
-        phantom.debug("JSON file liste: {}".format(json_files))
-        phantom.debug("Vault ID file liste: {}".format(vault_id_list))
-        outputs["vault_id_list"] = vault_id_list
-        return vault_id_list
 
     file_liste = re.findall(r'\b\w+\.json\b', pull_response)
     
     phantom.debug("file_liste: {}".format(file_liste))
-    
+    vault_id_list = []
     if file_liste:
         for file_name in file_liste:
             phantom.debug("for 1: {}".format(file_name))
             for root, directories, file in os.walk(repo_path_local):
                 if file_name in file:
                     voller_pfad = os.path.join(root, file_name)
-                    file_location = voller_pfad
-                    phantom.debug("File Name: {}".format(file_name))
-                    phantom.debug("voller_pfad: {}".format(file_location))
+                    success, message, vault_id = phantom.vault_add(container=None, file_location=voller_pfad, file_name=file_name, metadata=None, trace=False)
+                    vault_id_list.append(vault_id)
     else:
         phantom.debug("file_liste is emty")       
     
-    #list_json_files(repo_path_local)
+    outputs["vault_id_list"] = vault_id_list
     
         
     # Return a JSON-serializable object
