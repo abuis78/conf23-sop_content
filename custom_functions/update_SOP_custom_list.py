3b604cb7f700381c0bead50d198b22d5b01cb9ab
andreas buis <abuis@splunk.com>
2023-07-16 22:27:59 +0000

    sss

---
+++ custom_functions/update_SOP_custom_list.py
@@ -0,0 +1,30 @@
+def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filter=None, **kwargs):
+    """
+    Args:
+        artifact_id_list
+        container_id
+        prefix_filter
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    
+    for a in artifact_id_list:
+        url_filter = '?_filter_container='+ str(container_id) + '_filter_name__icontains="' + prefix_filter +'"'
+        r_url = phantom.build_phantom_rest_url('artifact')
+        r = phantom.requests.get(r_url,verify=False)
+        d = r.json()
+        v = d["version"]
+        phantom.debug(f"Version {v}")
+        
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
