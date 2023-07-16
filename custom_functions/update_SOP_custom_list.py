a6d2996732f764b1a70b7b7f4c5b24115c5ab3f2
andreas buis <abuis@splunk.com>
2023-07-16 23:11:13 +0000

    sss

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -33,7 +33,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
             ln = r2.json()
             phantom.debug(ln["content"])
             for sublist in ln["content"]:
-                if sublist[0] == n:
+                if n in sublist:
                     phantom.debug(f"Sublist: {sublist}")
         
     
