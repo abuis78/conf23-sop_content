ded338683126f9723ebb6f121f68cbf8adb7cb13
andreas buis <abuis@splunk.com>
2023-07-16 22:29:10 +0000

    sss

---
+++ dev_test.py
@@ -0,0 +1,61 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'update_sop_custom_list_1' block
+    update_sop_custom_list_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def update_sop_custom_list_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_sop_custom_list_1() called")
+
+    parameters = []
+
+    parameters.append({
+        "artifact_id_list": [2298],
+        "container_id": 33526,
+        "prefix_filter": "SOP",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="conf23-sop_content/update_SOP_custom_list", parameters=parameters, name="update_sop_custom_list_1")
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
