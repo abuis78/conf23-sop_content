318adcedbecc852a41af0135adc221667f75c1c2
andreas buis <abuis@splunk.com>
2023-07-16 18:30:53 +0000

    ddd

--- A001_get_JSON_file_add_new_SOP.py
+++ A001_get_JSON_file_add_new_SOP.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'git_list_files_1' block
-    git_list_files_1(container=container)
+    # call 'call_api_8' block
+    call_api_8(container=container)
 
     return
 
@@ -240,7 +240,7 @@ def git_list_files_1(action=None, success=None, container=None, results=None, ha
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="conf23-sop_content/git_list_files", parameters=parameters, name="git_list_files_1", callback=debug_4)
+    phantom.custom_function(custom_function="conf23-sop_content/git_list_files", parameters=parameters, name="git_list_files_1")
 
     return
 
@@ -297,14 +297,10 @@ def playbook_i002_check_sop_version_1(action=None, success=None, container=None,
 def debug_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("debug_4() called")
 
-    git_list_files_1__result = phantom.collect2(container=container, datapath=["git_list_files_1:custom_function_result.data.file_list"])
-
-    git_list_files_1_data_file_list = [item[0] for item in git_list_files_1__result]
-
     parameters = []
 
     parameters.append({
-        "input_1": git_list_files_1_data_file_list,
+        "input_1": None,
         "input_2": None,
         "input_3": None,
         "input_4": None,
@@ -327,6 +323,23 @@ def debug_4(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_4")
+
+    return
+
+
+@phantom.playbook_block()
+def call_api_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("call_api_8() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
 
