a5f5d3c4958c1796ccd08f32b4a9c299fe5438a4
andreas buis <abuis@splunk.com>
2023-07-17 06:21:54 +0000

    er

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -18,6 +18,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
     # Write your custom code here...
     
     for a in artifact_id_list:
+        phantom.debug(f"Artifact ID: {a}")
         url_filter = '/'+ str(a) + '?_filter_name__icontains="' + str(prefix_filter) +'"'
         r_url = phantom.build_phantom_rest_url('artifact')
         url = r_url + url_filter
@@ -46,7 +47,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
                         r3 = phantom.requests.post(r_url3, json=data, verify=False).json()
                         phantom.debug(r3)
         elif v_id is  None:
-            phantom.debug(f"Artifact ID: {v_id}")
+            
             phantom.debug(f"-------")
             phantom.debug(f"SOP is not in Custom List: {list_name}. The SOP {n} is now created in the list")
             r_url4 = phantom.build_phantom_rest_url('decided_list',list_name)
