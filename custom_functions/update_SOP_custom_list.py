115c6457aa1d81a150977170a3369fb2e842c8f7
andreas buis <abuis@splunk.com>
2023-07-17 10:32:13 +0000

    er

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -24,36 +24,39 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
         url = r_url + url_filter
         r = phantom.requests.get(url,verify=False)
         v = r.json()
-        v_id = v.get[0].get('cef', {}).get('version')
-        phantom.debug(f"------------{v}----------------\n")
-        n = v.get('cef', {}).get('name')
-        phantom.debug(f"v_id: {v}\n")
-        if v_id is not None:
-            r_url2 = phantom.build_phantom_rest_url('decided_list',list_name)
-            r2 = phantom.requests.get(r_url2,verify=False)
-            ln = r2.json()
+        #v_id = v.get[0].get('cef', {}).get('version')
+        #phantom.debug(f"------------{v}----------------\n")
+        #n = v.get('cef', {}).get('name')
+        for item in v['data']:
+            phantom.debug(f"v_id: {item['cef']['version']}\n")
+            v_id = item['cef']['version']
+            n = item['cef']['version']
+            if v_id is not None:
+                r_url2 = phantom.build_phantom_rest_url('decided_list',list_name)
+                r2 = phantom.requests.get(r_url2,verify=False)
+                ln = r2.json()
             
-            for i, sublist in enumerate(ln["content"]):
-                if n in sublist[0]:
-                    if int(v_id) <= int(sublist[1]):
-                        phantom.debug(f"Nothing to Update for SOP: {n} this one is still up to date")
-                    elif int(v_id) > int(sublist[1]):
-                        phantom.debug(f"SOP Update")
-                        phantom.debug(f"Element was found in row {i + 1} ")
-                        row = i
-                        r_url3 = phantom.build_phantom_rest_url('decided_list',list_name)
-                        sublist[1] = str(v_id) 
-                        l_e = sublist
-                        data = { "update_rows": { row : sublist }}
-                        phantom.debug(f"New Data: {data}")
-                        r3 = phantom.requests.post(r_url3, json=data, verify=False).json()
-                        phantom.debug(r3)
-                    else:
-                        phantom.debug("SOP ist not in list")
+                for i, sublist in enumerate(ln["content"]):
+                    if n in sublist[0]:
+                        if int(v_id) <= int(sublist[1]):
+                            phantom.debug(f"Nothing to Update for SOP: {n} this one is still up to date")
+                        elif int(v_id) > int(sublist[1]):
+                            phantom.debug(f"SOP Update")
+                            phantom.debug(f"Element was found in row {i + 1} ")
+                            row = i
+                            r_url3 = phantom.build_phantom_rest_url('decided_list',list_name)
+                            sublist[1] = str(v_id) 
+                            l_e = sublist
+                            data = { "update_rows": { row : sublist }}
+                            phantom.debug(f"New Data: {data}")
+                            r3 = phantom.requests.post(r_url3, json=data, verify=False).json()
+                            phantom.debug(r3)
+                        else:
+                            phantom.debug("SOP ist not in list")
             
-        elif v_id is  None:
-            phantom.debug(f"-------")
-            phantom.debug(f"It's not SOP Artifact {n}")
+            elif v_id is  None:
+                phantom.debug(f"-------")
+                phantom.debug(f"It's not SOP Artifact {n}")
             
         
     
