2cc6a16d82cad951802a1f0f8f9fb36d73dc19f9
andreas buis <abuis@splunk.com>
2023-07-17 02:26:52 +0000

    sss

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -39,7 +39,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
                         phantom.debug(f"Element was found in row {i + 1} ")
                         r_url3 = phantom.build_phantom_rest_url('decided_list',list_name)
                         sublist[1] = str(v_id) 
-                        data = { "content": sublist,"name": list_name}
+                        data = { "content": sublist,"name": str(list_name) }
                         phantom.debug(f"New Data: {data}")
                         r3 = phantom.requests.post(r_url3, json=data, verify=False).json()
                         phantom.debug(r3)
