74c6cef395dc3447ded2cd6fd621e60b6b6e6714
andreas buis <abuis@splunk.com>
2023-07-17 10:04:04 +0000

    er

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -26,7 +26,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
         v = r.json()
         v_id = v.get[0].get('cef', {}).get('version')
         phantom.debug(f"------------{v}----------------\n")
-        n = v.get('cef', {}).get('name')
+        n = v.get('cef').get('name')
         phantom.debug(f"v_id: {v}\n")
         if v_id is not None:
             r_url2 = phantom.build_phantom_rest_url('decided_list',list_name)
