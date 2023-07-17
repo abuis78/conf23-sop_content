3daa3c524d98829aff20c924b77b5ff4e4383eaa
andreas buis <abuis@splunk.com>
2023-07-17 11:55:26 +0000

    er

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -43,7 +43,8 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
                     
                     if n in sublist[0]:
                         phantom.debug(f"{n} ist in Subliste - in Zeile {i}")
-                    elif i == "":
+                    # elif i == "":
+                    else:
                         phantom.debug(f"N ist NICHT in Subliste {n} - in Zeile {i}")
                     """
                     if n in sublist[0]:
