82177c75e63cfe8ab3666928b0660c28aae6e078
andreas buis <abuis@splunk.com>
2023-07-18 15:50:47 +0000

    ss

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -71,7 +71,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
         else:
             phantom.debug("Die Liste 'data' ist leer.")
 
-        phantom.debug(f"request r {data}")
+
         
     
                           
