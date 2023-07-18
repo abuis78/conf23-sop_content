291103ebec638eeccc1acf7ede9eb4b42bd9f325
andreas buis <abuis@splunk.com>
2023-07-18 13:12:52 +0000

    ss

--- custom_functions/update_SOP_custom_list.py
+++ custom_functions/update_SOP_custom_list.py
@@ -60,7 +60,7 @@ def update_SOP_custom_list(artifact_id_list=None, container_id=None, prefix_filt
 
     def create_update_workbook(task,name,json,a):
         
-        data = json.loads(json)
+        data = json.loads()
         url = phantom.build_phantom_rest_url('workbook_template')
         response_data = phantom.requests.post(url, json=data, verify=False)
         phantom.debug(response_data)
